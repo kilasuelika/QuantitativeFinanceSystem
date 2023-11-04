c4673651b3ef78667574f56e1930c53420d1a4bd
Kila <semeego@live.com>
2023-11-04 22:54:23 +0800

    更新架构

--- QuantitativeFinanceSystem/DataServer/main.cpp
+++ QuantitativeFinanceSystem/DataServer/main.cpp
@@ -1,161 +1,24 @@
-#include <websocketpp/config/asio_no_tls.hpp>
-#include <websocketpp/server.hpp>
-#include <websocketpp/client.hpp>
+#include <boost/program_options.hpp>
 
-#include <functional>
-//#include <fmt/format.h>
-#include <format>
-typedef websocketpp::server<websocketpp::config::asio> server;
-typedef websocketpp::client<websocketpp::config::asio> client;
+#include <boost/program_options/options_description.hpp>
+#include <boost/program_options/parsers.hpp>
+#include <boost/program_options/positional_options.hpp>
+#include <boost/program_options/variables_map.hpp>
 
-class Server
-{
-public:
-    Server()
-    {
-        // Set logging settings
-        s_.set_error_channels(websocketpp::log::elevel::all);
-        s_.set_access_channels(websocketpp::log::alevel::all ^ websocketpp::log::alevel::frame_payload);
+#include "include/DataServer.h"
 
-        // Initialize Asio
-        s_.init_asio();
+namespace po = boost::program_options;
+int main(int argc, char** argv) {
+    boost::program_options::options_description desc("Allowed options");
+    desc.add_options()
+        ("config", po::value< std::string >()->default_value("config.json"), ".json config file");
 
-        //指定回调函数
-        s_.set_open_handler(bind(&Server::on_open, this, std::placeholders::_1));
-        s_.set_close_handler(bind(&Server::on_close, this, std::placeholders::_1));
-        s_.set_message_handler(bind(&Server::on_message, this, std::placeholders::_1, std::placeholders::_2));
-    }
+    po::variables_map vm; //容器
+    po::store(po::parse_command_line(argc, argv, desc), vm);  //执行解析
+    po::notify(vm);  //一定要加上这一句
 
-    //在open时存储连接，便于后续主动发送消息
-    void on_open(websocketpp::connection_hdl hdl)
-    {
-        std::cout << std::format("New connection\n");
-        con = hdl;
-    }
+    std::string config = vm["config"].as<std::string>();
 
-    void on_close(websocketpp::connection_hdl hdl)
-    {
-        // Handle a connection closing
-    }
-
-    void on_message(websocketpp::connection_hdl hdl, server::message_ptr msg)
-    {
-        //读取内容
-        std::print(std::cout, "[Server]: new message from client. Content: {}\n", msg->get_payload());
-    }
-    void run()
-    {
-        // Listen on port 9002
-        s_.listen(9002);
-
-        // Queues a connection accept operation
-        s_.start_accept();
-
-        // Start the Asio io_service run loop
-        s_.run();
-    }
-    void send_message(const std::string& message)
-    {
-        websocketpp::lib::error_code ec;
-        s_.send(con, message, websocketpp::frame::opcode::text, ec);
-
-        if (ec)
-        {
-            std::cout << "Send Failed because: " << ec.message() << std::endl;
-        }
-        else
-        {
-            std::cout << "Send Success" << std::endl;
-        }
-    }
-
-private:
-    server s_;
-    websocketpp::connection_hdl con;
-};
-
-
-
-
-class Client
-{
-public:
-
-
-    Client()
-    {
-        // Set logging to be pretty verbose (everything except message payloads)
-        c_.set_access_channels(websocketpp::log::alevel::all);
-        c_.clear_access_channels(websocketpp::log::alevel::frame_payload);
-
-        // Initialize ASIO
-        c_.init_asio();
-
-        // Register our message handler
-        c_.set_message_handler([this](websocketpp::connection_hdl hdl, client::message_ptr msg) {
-            on_message(hdl, msg);
-            });
-    }
-
-    void on_message(websocketpp::connection_hdl hdl, client::message_ptr msg)
-    {
-        std::print(std::cout, "[Client] New message from server: {}\n", msg->get_payload());
-    }
-
-    void run(std::string const& uri)
-    {
-        websocketpp::lib::error_code ec;
-
-        con = c_.get_connection(uri, ec);
-
-        if (ec)
-        {
-            std::cout << "could not create connection because: " << ec.message() << std::endl;
-            return;
-        }
-
-        c_.connect(con);
-        c_.run();
-    }
-
-    void send_message(const std::string& message)
-    {
-        websocketpp::lib::error_code ec;
-        c_.send(con, message, websocketpp::frame::opcode::text, ec);
-
-        if (ec)
-        {
-            std::cout << "Send Failed because: " << ec.message() << std::endl;
-        }
-        else
-        {
-            std::cout << "Send Success" << std::endl;
-        }
-    }
-private:
-    client c_;
-    client::connection_ptr con;
-};
-
-int main()
-{
-    Server s;
-    auto server = std::async(std::launch::async, [&]() {s.run(); });
-
-    //先睡眠一下，等待服务端启动
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    Client c;
-    auto client = std::async(std::launch::async, [&]() {c.run("ws://127.0.0.1:9002"); });
-
-    //从客户端发送消息
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    c.send_message("Hello, ");
-
-    //从服务端发送消息
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    s.send_message("world!");
-
-    server.get();
-    client.get();
-    return 0;
+    DataServer server;
+    server.Run(config);
 }
--- QuantitativeFinanceSystem/DataServer/src/DataServer.cpp
+++ QuantitativeFinanceSystem/DataServer/src/DataServer.cpp
@@ -1,6 +1,9 @@
 #include "../include/DataServer.h"
 
-DataServer::DataServer(int port)
-    :_port(port) {
-
+bool DataServer::Run(const std::string& configfile) {}
+void DataServer::RegisterListener(websocketpp::connection_hdl hdl) {}
+void DataServer::on_open(websocketpp::connection_hdl hdl) {}
+void DataServer::on_close(websocketpp::connection_hdl hdl) {}
+void DataServer::on_message(websocketpp::connection_hdl hdl,
+    SocketServer::message_ptr msg) {
 }
